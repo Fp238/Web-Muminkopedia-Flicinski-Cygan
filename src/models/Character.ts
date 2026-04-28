c581a3ddc9b14db4b9f0e8924eafa5e2e7640bde
Filip <filip.flicinski@cyfroweszkoly.pl>
2026-04-28 18:42:34 +0200

    dodanie modelu character

---
+++ src/models/Character.ts
@@ -0,0 +1,42 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface ICharacter extends Document {
+    name: string;
+    description: string;
+    species: "Muminek" | "Miukk" | "Paszczak" | "Inne";
+    isHibernating: boolean;
+    bestFriend?: string | null;
+}
+
+const CharacterSchema = new Schema<ICharacter>(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        species: {
+            type: String,
+            required: true,
+            enum: ["Muminek", "Miukk", "Paszczak", "Inne"],
+        },
+        isHibernating: {
+            type: Boolean,
+            default: false,
+        },
+        bestFriend: {
+            type: Schema.Types.ObjectId,
+            ref: "Character",
+            default: null,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Character = model<ICharacter>("Character", CharacterSchema);
